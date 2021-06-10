c69c0b7692b266b1192f549c7edfba83e21b5d4f
Bjørn Dons <guardsmanpanda@gmail.com>
2021-06-10 19:08:39 +0200

    dep update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.postCss('resources/app.css', 'public/static/dist/app.css', [require('tailwindcss')])
-mix.js('resources/app.js', 'public/static/dist/app.js')
+mix.postCss('resources/app.css', 'public/static/dist/app.css', [require('tailwindcss')]).version()
+mix.js('resources/app.js', 'public/static/dist/app.js').version()
