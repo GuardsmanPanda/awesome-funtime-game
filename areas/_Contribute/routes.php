87f0a6527265c4797786e6775c056795066a49b5
Bjørn Dons <guardsmanpanda@gmail.com>
2021-08-08 21:13:10 +0200

    allow all to rate images.

--- areas/_Contribute/routes.php
+++ areas/_Contribute/routes.php
@@ -25,14 +25,7 @@ Route::middleware('permission:contribute')->group(function () {
 });
 
 Route::post('/rate/{panorama_id}/{rating}', function (string $panorama_id, int $rating) {
-    $test = DB::selectOne("
-        SELECT p.panorama_id FROM panorama p
-        LEFT JOIN round r ON r.panorama_id = p.panorama_id
-        LEFT JOIN round_user ru on r.id = ru.round_id
-        WHERE p.panorama_id = ? AND ru.user_id = ?", [$panorama_id, Auth::$user_id]);
-    if ($test === null || $rating < 1 || $rating > 7) {
-        throw new InvalidArgumentException("Not allowed to rate this panorama");
-    }
+    abort_if(Auth::$user_id === -1, 403, 'Must be logged in to rate images.');
     DB::insert("
         INSERT INTO panorama_rating (panorama_id, user_id, rating) VALUES (?, ?, ?)
         ON CONFLICT (panorama_id, user_id) DO UPDATE SET rating = excluded.rating
